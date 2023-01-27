343993fdf501ae9cdf4e9d94594d256b9a7dc403
gdtknight <gdtknight@naver.com>
2023-01-27 14:11:29 +0900

    comment ServiceProperties.java
    library 에 의존하는 application을 작성할 때, 해당 application에서 사용하는
    
    application.properties 파일과 충돌가능성이 있으므로 library project에는
    
    application.properties 파일을 사용하더라도 향후 build 과정에서 이를 제외시켜야 한다.

--- library/src/main/java/com/example/multimodule/service/ServiceProperties.java
+++ library/src/main/java/com/example/multimodule/service/ServiceProperties.java
@@ -2,6 +2,13 @@ package com.example.multimodule.service;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/**
+ * application.properties 를 src/main/resources/ 아래에 추가할 수 있지만
+ * 이렇게 할 경우 차후에 library 를 이용하는 application에서 application.properties를 이용할 경우
+ * 충돌이 발생할 가능성이 있다. 따라서 개발 시점에는 src/main/resources/ 디렉토리에 있는
+ * application.properties를 이용하고 build 시에는 해당 파일을 제외하도록 build tool을 설정항여야 한다.
+ */
+
 @ConfigurationProperties("service")
 public class ServiceProperties {
 
@@ -18,3 +25,4 @@ public class ServiceProperties {
     this.message = message;
   }
 }
+
