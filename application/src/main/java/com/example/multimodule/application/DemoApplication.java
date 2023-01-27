cbe9265999f8e3652e1cf5183eabe66dc16aaeb0
gdtknight <gdtknight@naver.com>
2023-01-27 15:03:30 +0900

    add comment
    application project 에서 library project의 MyService Class 를 사용하기 위해서 필요한 설정 설명
    
    둘의 package 경로가 다르기 때문에 3가지 방법을 통해 Import 가 가능하다.
    
    - `@Import` 애너테이션
    
    - `@SpringBootApplication(scanBasePackageClasses={})` 애너테이션을 이용하여 직접 패키지 경로를 설정
    
    - `scanBasePackageClasses`에 공통의 상위 패키지명을 설정
    
    해당 프로젝트에서는 3번째 방법을 통해 scanBasePackageClasses="com.example.multimodule" 의 형태로 설정하였다.

--- application/src/main/java/com/example/multimodule/application/DemoApplication.java
+++ application/src/main/java/com/example/multimodule/application/DemoApplication.java
@@ -6,6 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Because DemoApplication is inside a different package
+ * (com.example.multimodule.application) than MyService (com.example.multimodule.service),
+ * @SpringBootApplication cannot automatically detect it.
+ *
+ * There are different ways to let `MyService be picked up:
+ * - Import it directly with @Import(MyService.class).
+ * - Fetch everything from its package by using @SpringBootApplication(scanBasePackageClasses={…}).
+ * - Specifying the parent package by name: com.example.multimodule. (This guide uses this method)
+ */
+
 @SpringBootApplication(scanBasePackages = "com.example.multimodule")
 @RestController
 public class DemoApplication {
